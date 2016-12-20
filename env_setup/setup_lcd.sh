04d1536dfceb53912bdfb053b3105e70096bd64c
Marty Chao <marty@browan.com>
2016-12-20 11:22:27 +0000

    ada-lcd env setup

--- env_setup/setup_lcd.sh
+++ env_setup/setup_lcd.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-echo "dtparam=i2c_arm=on
-dtparam=i2c1=on" >> /boot/config.txt
-echo "i2c-bcm2708
-i2c-dev" >> /etc/modules
-pip install Adafruit-GPIO Adafruit-PureIO Adafruit-CharLCD
+sudo apt install python-dev
+sudo pip install Adafruit-GPIO Adafruit-PureIO Adafruit-CharLCD
