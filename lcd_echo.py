a651d64da3fae5bdbb070225cce534535e8a0190
marty @ raspberry pi <marty@freedom-tek.om>
2021-08-13 15:03:33 +0800

    upgrade to new lcd module version

--- lcd_echo.py
+++ lcd_echo.py
@@ -1,12 +1,18 @@
-#!/usr/bin/python
-
-import Adafruit_CharLCD as LCD
+#!/usr/bin/env python3
 import sys
 from subprocess import *
 from time import sleep, strftime
 from datetime import datetime
+import board
+import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
 
-lcd = LCD.Adafruit_CharLCDPlate()
+# Modify this if you have a different sized Character LCD
+lcd_columns = 16
+lcd_rows = 2
 
+# Initialise I2C bus.
+i2c = board.I2C()  # uses board.SCL and board.SDA
+
+lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
 lcd.clear()
-lcd.message(str(sys.argv[1])+str(sys.argv[2]))
+lcd.message = str(sys.argv[1])+str(sys.argv[2])
