071eb6491486fb08e583de028f99c94a0aac6b11
marty @ raspberry pi <marty@freedom-tek.om>
2021-08-18 15:56:46 +0800

    upgrade module to "adafruit-circuitpython-charlcd   3.3.10"

--- env_setup/setup_lcd.sh
+++ env_setup/setup_lcd.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo apt install python-dev
-sudo pip install Adafruit-GPIO Adafruit-PureIO Adafruit-CharLCD
+# sudo apt install python-dev
+# sudo pip install Adafruit-GPIO Adafruit-PureIO Adafruit-CharLCD
+sudo pip3 install adafruit-circuitpython-charlcd
