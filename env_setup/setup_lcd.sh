68ae914b6ccaff60341dff0a14aaf180b7285adc
Marty@MBA <marty@freedom-tek.com>
2025-05-24 18:36:35 +0800

    add:old man

--- env_setup/setup_lcd.sh
+++ env_setup/setup_lcd.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Install Poetry
+curl -sSL https://install.python-poetry.org | python3 -
+# Add Path 
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
 # sudo apt install python-dev
 # sudo pip install Adafruit-GPIO Adafruit-PureIO Adafruit-CharLCD
 sudo pip3 install adafruit-circuitpython-charlcd
